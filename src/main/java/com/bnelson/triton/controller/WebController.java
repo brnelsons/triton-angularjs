1d9301e1fe75457685799d5381a1605dd1fffafd
bnelson <brnelsons139@gmail.com>
2017-11-09 17:16:46 -0600

    potential config path system

--- src/main/java/com/bnelson/triton/controller/WebController.java
+++ src/main/java/com/bnelson/triton/controller/WebController.java
@@ -2,13 +2,14 @@ package com.bnelson.triton.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 /**
  * Created by brnel on 11/3/2017.
  * TODO fill out class documentation
  */
 @Controller
-public class UrlController {
+public class WebController {
 
     @GetMapping("/")
     public String homepage(){
@@ -23,5 +24,11 @@ public class UrlController {
     @GetMapping("/add")
     public String nav(){
         return "add";
+    }
+
+    @GetMapping("/config/{gameName}/{serverName}")
+    public String nav(@PathVariable("gameName") String gameName,
+                      @PathVariable("serverName") String serverName){
+        return "config";
     }
 }
